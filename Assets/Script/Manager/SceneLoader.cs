c4dd252444c25a87a18fcaeecc911343ea83780f
TendouHakai <thanhconghoahai@gmail.com>
2023-08-20 17:23:11 +0700

    complete coding portal to change scene/ SceneLoader to manager change scene and effect change scene

--- Assets/Script/Manager/SceneLoader.cs
+++ Assets/Script/Manager/SceneLoader.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static SceneLoader instance;
+
+    public static SceneLoader getInstance()
     {
-        
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType<SceneLoader>();
+        }
+        return instance;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [SerializeField] public Vector3 playerPos;
+    [SerializeField] public Player player;
+    [SerializeField] public Animator ani;
+
+    [Header("----------Time----------")]
+    [SerializeField] float timeChangeScene;
+    [SerializeField] float timeSetPlayerPos;
+    float timeStart = 0f;
+    bool isChangeScene = false;
+    bool isSetplayerPos = false;
+
+    int sceneNumber;
+
+    private void Update()
+    {
+        if(isChangeScene)
+        {
+            if (timeStart > timeChangeScene)
+            {
+                isChangeScene = false;
+                isSetplayerPos = true;
+
+                SceneManager.LoadScene(sceneNumber);
+
+                timeStart = 0f;
+            }
+            else timeStart += Time.deltaTime;
+        }
+
+        if(isSetplayerPos)
+        {
+            if (timeStart > timeSetPlayerPos)
+            {
+                isSetplayerPos = false;
+                setPlayerPosition();
+                timeStart = 0f;
+            }
+            else timeStart += Time.deltaTime;
+        }
+    }
+
+    public void loadScene(int sceneNumber, Vector3 playerPos)
+    {
+        ani.Play("Sceneloader_START_CHANGE_SCENE");
+
+        this.sceneNumber = sceneNumber;
+        this.playerPos = playerPos;
+        isChangeScene=true;
+        timeStart = 0f;
+    }
+
+    public void setPlayerPosition()
+    {
+        player.transform.position = playerPos;
     }
 }
--- Assets/Script/non-moving object/Portal.cs
+++ Assets/Script/non-moving object/Portal.cs
@@ -8,14 +8,11 @@ public class Portal : MonoBehaviour
     [SerializeField] int sceneNumber;
     [SerializeField] Vector3 posPlayer;
 
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            SceneManager.LoadScene(sceneNumber);
-
-            collision.transform.position = posPlayer;
+            SceneLoader.getInstance().loadScene(sceneNumber, posPlayer);
         }
     }
 }
